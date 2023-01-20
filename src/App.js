b0fb78e348529fb4c0e25d6562baa7cba38e5e71
Norah <noura.ah1493@gmail.com>
2023-01-20 05:45:13 +0300

    delete route

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import Main from './pages/Main';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
   return (
     <div className="App">            
       <ResponsiveAppBar/>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/personal-website' element={<Main />} />   
-        </Routes>
-      </BrowserRouter>
+      <Main/>
     </div>
   );
 }
